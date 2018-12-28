414558c441910b47f9018d84b8da630534e3c487
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2018-12-28 09:40:55 -0500

    Fixed icon builder to make it possible to work with 2-color templates; added new icon

--- etc/build.sh
+++ etc/build.sh
@@ -9,13 +9,17 @@ tmpfile=$(mktemp /tmp/xload.XXXXXXXXXX)
 mkdir -p ext/icons
 
 make_png () {
-    sed "s/fill:#[0-9a-fA-F]\{6\};/fill:$2/g" $1 |\
-        sed "s/stroke:#[0-9a-fA-F]\{6\};/stroke:$2/g" > $tmpfile
-    svgtoimg -g "$3" $tmpfile $4
+    # <1:source> <2:color_1> <3:color_2> <4:WxH> <5:target>
+    sed "s/fill:#1[0-9a-fA-F]\{5\};/fill:$2/g"   $1 |\
+    sed "s/stroke:#1[0-9a-fA-F]\{5\};/stroke:$2/g"    |\
+    sed "s/fill:#2[0-9a-fA-F]\{5\};/fill:$3/g"      |\
+    sed "s/stroke:#2[0-9a-fA-F]\{5\};/stroke:$3/g"    > $tmpfile
+    svgtoimg -g "$4" $tmpfile $5
 }
 
-make_png assets/icon_na.svg '#A00000' 128,128 ext/icons/icon_128_na.png
-make_png assets/icon.svg    '#008000' 128,128 ext/icons/icon_128_green.png
-make_png assets/icon.svg    '#0000B0' 128,128 ext/icons/icon_128_blue.png
+make_png assets/icon_na.svg  '#A00000'  X         128,128 ext/icons/icon_128_na.png
+make_png assets/icon_err.svg '#008000' '#A00000'  128,128 ext/icons/icon_128_err.png
+make_png assets/icon.svg     '#008000'  X         128,128 ext/icons/icon_128_green.png
+make_png assets/icon.svg     '#0000B0'  X         128,128 ext/icons/icon_128_blue.png
 
 rm -r $tmpfile
