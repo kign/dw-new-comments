743ca83691b5613c2ee5ac34149769eeb38e7287
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2019-03-25 22:43:35 -0400

    Added makeSVGElm, made it possible to use entities with makeElm()

--- ext/scripts/ignlib.js
+++ ext/scripts/ignlib.js
@@ -1,5 +1,11 @@
 'use strict'
 
+// Ordinarely, when creating a text element with an HTML entity such as
+// "&npsp;" method createTextNode doesn't interpret entities, so "&npsp;"
+// would be text you get
+// Thus we are using innerHTML when the only argument is text
+// makeElm('DIV', {}, "&nbsp;") will create DIV with space
+// makeElm('DIV', {}, "", "&nbsp;") will create DIV with text "&nbsp;"
 function makeElm (tag, attrs, ...children) {
   let node = document.createElement(tag);
 
@@ -8,14 +14,39 @@ function makeElm (tag, attrs, ...children) {
       node.setAttribute(k, attrs[k]);
   }
 
-  for (let c of children) {
-    if (typeof c == "string")
-      node.appendChild(document.createTextNode(c));
-    else
-      node.appendChild(c);
+  if (children.length == 1 && typeof children[0] == "string")
+    node.innerHTML = children[0];
+  else {
+    for (let c of children) {
+      if (typeof c == "string" || typeof c == "number")
+        node.appendChild(document.createTextNode(c));
+      else
+        node.appendChild(c);
+    }
   }
 
   return node;
+}
+
+function makeSVGElm (tag, attrs, ...children) {
+  const ns = "http://www.w3.org/2000/svg";
+  let node = document.createElementNS(ns, tag);
+
+  for (let k in attrs)
+    node.setAttribute(k, attrs[k]);
+
+    for (let c of children)
+      if (typeof c == "string" || typeof c == "number")
+        node.appendChild(document.createTextNode(c));
+      else
+        node.appendChild(c);
+
+  return node;
+}
+
+function eraseChildren(node) {
+  while (node.firstChild)
+    node.removeChild(node.firstChild);
 }
 
 function xpath_do(elm, xp, action) {
